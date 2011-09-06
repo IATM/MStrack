988ca2c21a8c30355191cba194940f33787675ec
Simon Rascovsky <simonmd@gmail.com>
2011-09-06 18:18:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MStrack::Application.config.secret_token = '9b896db5d56f6f20dc8e3960fc89ea1026ae4b3a00fed521f583890c23aaf5297a0f9f2361a749c8afeda9b4eb530f5fca8877ec22ca6c1fcf61e43a0f76b4fd'
