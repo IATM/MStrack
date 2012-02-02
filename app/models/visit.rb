ed6be239ff5ad95d38890af800167e51b8126402
gABoCas <1gabocas1@gmail.com>
2012-02-02 17:51:58 -0500

    Logica de modelos de examenes fisicos y sidebar

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,8 @@ class Visit < ActiveRecord::Base
   belongs_to :user
   belongs_to :patient
   has_one :enfermedad_actual
+  has_one :ef_columna
+  has_one :ef_general
+  has_one :ef_mental
+  has_one :ef_psiqu
 end
